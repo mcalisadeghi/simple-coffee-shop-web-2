4aed5eb953dcc78f7adab0dd2d28b9f4168b09da
mcalisadeghi <ali.ke970@gmail.com>
2023-12-21 17:26:32 +0330

    add closeMenuBtn

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@ const header = document.querySelector("header");
 const menubtn = document.querySelector("#menu-btn");
 const closeMenuBtn = document.querySelector("#close-menu-btn");
 
+// toggle mobile menu on menu button click
 menubtn.addEventListener("click",()=>{
     header.classList.toggle("shwo-mobile-menu");
-})
+});
+
+// close mobile menu on close button click
+closeMenuBtn.addEventListener("click",()=>{
+    menubtn.click();
+});
